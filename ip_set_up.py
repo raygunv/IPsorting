cfc3e810bf4b94ac84ae55c7915a4559b28c2d93
raygunv <reagan@vaughn.net>
2016-02-07 20:54:32 -0600

    Update ip_set_up.py

--- ip_set_up.py
+++ ip_set_up.py
@@ -6,7 +6,10 @@ from collections import defaultdict
 import Queue
 import threading
 
-
+"""
+This code uses a given txt file to access certain tables in a database and from those tables retrieves a list of ips which
+are then pinged to determine if they are still in use. This new data is then uploaded back into the database
+"""
 
 def get_ping(hostname_list,tag, timeout):
 
@@ -17,15 +20,15 @@ def get_ping(hostname_list,tag, timeout):
 	counts = 0
 	for hostname in hostname_list:
 		if hostname[0] not in ip_time_dict:
-			ip_time_dict[hostname[0]] = 0
+			ip_time_dict[hostname[0]] = 0 #if its a new hostname, initialize it to 0
 		ip_val_dict[hostname[0]] = hostname[1]
-		if platform.system() == "Windows":
+		if platform.system() == "Windows": #different for different operating systems
 			command="ping "+hostname[0]+" -n 1 -w "+str(timeout*250)
 		else:
 			command="ping -i "+str(timeout)+" -c 1 " + hostname[0]
 		proccess = subprocess.Popen(command, stdout=subprocess.PIPE)
 		matches=re.match('.*time=([0-9]+)ms.*', proccess.stdout.read(),re.DOTALL)
-		if matches:
+		if matches: #if the ping succeeded
 			#print hostname[0], matches.group(1)
 			counter+=1
 			ip_val_dict[hostname[0]] = 0
@@ -33,9 +36,9 @@ def get_ping(hostname_list,tag, timeout):
 		else: 
 			if ip_val_dict[hostname[0]] == 0:
 				ip_time_dict[hostname[0]] +=1
-				if ip_time_dict[hostname[0]] > 299:
+				if ip_time_dict[hostname[0]] > 299: #if it has been more than 300 days since last successful ping
 					if int(hostname[0].split('.')[3]) < 70:
-						ip_val_dict[hostname[0]] = 2
+						ip_val_dict[hostname[0]] = 2 #2 is business, 1 is customer
 					else:
 						ip_val_dict[hostname[0]] = 1
 			else:
@@ -48,7 +51,7 @@ def get_ping(hostname_list,tag, timeout):
 
 
 def read_file(filename):
-
+#reads in a statndard file and splits it into a list of the ip and access point
 	ip_ap_list = []
 	f = open(str(filename), "r")
 	for line in f:
@@ -61,7 +64,7 @@ def read_file(filename):
 
 
 def read_IP(url):
-
+#opens a webpage based on the inputeted url
 
 	username = 'zstaff'
 	password = '***********'
@@ -78,16 +81,16 @@ def write_sql(data):
 
 	db = MySQLdb.connect("192.185.******","mrgood_zeecon","*********","mrgood_zipdb" )
 	cursor = db.cursor()
-	cursor.execute('DROP TABLE IF EXISTS %s' %data[1])
+	cursor.execute('DROP TABLE IF EXISTS %s' %data[1]) 
 
 	
 
 	sql ="""CREATE TABLE %s  (IP TEXT, AVAILIBILITY INT, TIME INT)""" %data[1]
 	cursor.execute(sql)
-	for key in data[0]:
+	for key in data[0]: #for each access point
 
 		sql = ("""INSERT INTO """ + data[1] + """ VALUES (%s,%s,%s)""")
-		cursor.execute(sql,(key, data[0][key], data[2][key]))
+		cursor.execute(sql,(key, data[0][key], data[2][key])) #inserts the ip and thr availibility of the ip into the table
 		db.commit()
 
 	
@@ -101,14 +104,14 @@ def fetch_ip_value_tags(table_names):
 	temp_list = []
 	ip_list = []
 
-	db = MySQLdb.connect("192.185.46.179","mrgood_zeecon","Z33c0nZ@dm1n","mrgood_zipdb" )
+	db = MySQLdb.connect("192.185.46.179","mrgood_zeecon","***********","***********" )
 	cursor = db.cursor()
 
 	for name in table_names:
 
 		cursor.execute("""SELECT * FROM %s""" %name)
 		ip_val_list = cursor.fetchall()
-		for item in ip_val_list:
+		for item in ip_val_list: #retrieves every ip in the table and adds it to a list of ips 
 			temp_list.append(item)
 
 		ip_list.append((temp_list,name))
@@ -139,17 +142,17 @@ for item in list_of_ip_ap:
 	if item[1] not in tag_list:
 		tag_list.append(item[1])
 
-fetched_items = fetch_ip_value_tags(tag_list)
+fetched_items = fetch_ip_value_tags(tag_list) #fetches the ips and their values
 
 counter = 0
 for item in fetched_items:
 	counter+=1
 	print counter
-	get_ping(item[0], item[1], 1)
+	get_ping(item[0], item[1], 1) #pings every ip
 
 
 for item in pinged_data:
 
-	write_sql(item)
+	write_sql(item)#adds the new data to the database
 
 
